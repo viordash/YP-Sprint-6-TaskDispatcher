e388f71d2f86f6003cf7d6c34465e3deb32fb0e4
Vitalii Orazov <viordash@yandex.ru>
2025-09-26 20:55:32 +0000

    BoundedQueue: implementation

--- include/queue/bounded_queue.hpp
+++ include/queue/bounded_queue.hpp
@@ -1,10 +1,21 @@
 #pragma once
 #include "queue/queue.hpp"
 
+#include <condition_variable>
+#include <functional>
+#include <mutex>
+#include <optional>
+#include <queue>
+
 namespace dispatcher::queue {
 
 class BoundedQueue : public IQueue {
-    // здесь ваш код
+private:
+    std::queue<std::function<void()>> queue;
+    std::mutex mutex;
+    std::condition_variable not_full;
+    int capacity;
+
 public:
     explicit BoundedQueue(int capacity);
 
